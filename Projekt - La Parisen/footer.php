b21e1ed5f264bc67beef25b48f3fa50db4c48a50
Slopyy <136683744+jSloparSlopy@users.noreply.github.com>
2026-06-12 11:21:26 +0200

    Projekt - Faza IV.

---
+++ Projekt - La Parisen/footer.php
@@ -0,0 +1,6 @@
+<footer>
+    <p><a href="index.php">Le Parisien</a> — Projekt iz programiranja web aplikacija</p>
+    <p>Kolegij: Programiranje web aplikacija, 4. semestar</p>
+    <p>Autor: Jan Šlopar — 0246123378</p>
+    <p><a href="https://github.com/jSloparSlopy/PWA/tree/main/Projekt%20-%20La%20Parisen" target="_blank">GitHub repozitorij</a></p>
+</footer>
